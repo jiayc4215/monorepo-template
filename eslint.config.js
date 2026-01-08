76ef740a9c05d8e0ae909cc07900cd3a47691d63
jiayingchao <2806242786@qq.com>
2026-01-08 20:54:39 +0800

    build(root): 添加eslint

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import js from "@eslint/js"; //js规范（标准的）
+import globals from "globals"; //环境
+import pluginVue from "eslint-plugin-vue"; //vue规范
+import { defineConfig } from "eslint/config"; //配置
+const ignores = ["**/dist/**", "**/node_modules/**", ".*"];
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,vue}"], //匹配文件
+    plugins: { js },
+    extends: ["js/recommended"], //js规范
+    languageOptions: { globals: { ...globals.browser, ...globals.node } }, //全局变量 window
+    ignores, //忽略文件
+    rules: {
+      "no-console": "error", //禁止console
+      semi: "error", //强制分号 {为了演示与prettier冲突}
+    },
+  },
+  pluginVue.configs["flat/essential"], //vue规范
+]);
