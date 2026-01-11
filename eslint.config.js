da6a57d7368539ae3003d5ea3006d30cee8a52da
jiayingchao <2806242786@qq.com>
2026-01-11 17:00:30 +0800

    feat(root): 初始化monmrepo 工作区

--- eslint.config.js
+++ eslint.config.js
@@ -5,13 +5,23 @@ import { defineConfig } from "eslint/config" //配置
 import eslintConfigPrettier from "eslint-config-prettier" // prettier
 const ignores = ["**/dist/**", "**/node_modules/**", ".*"]
 export default defineConfig([
+  pluginVue.configs["flat/essential"], //vue规范
   {
     files: ["**/*.{js,mjs,cjs,vue}"], //匹配文件
     plugins: { js },
     extends: ["js/recommended"], //js规范
-    languageOptions: { globals: { ...globals.browser, ...globals.node } }, //全局变量 window
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        uni: true
+      }
+    }, //全局变量 window
     ignores, //忽略文件
-    ...eslintConfigPrettier
-  },
-  pluginVue.configs["flat/essential"] //vue规范
+
+    ...eslintConfigPrettier,
+    rules: {
+      "vue/multi-word-component-names": "off"
+    }
+  }
 ])
